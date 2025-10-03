31935c6ac5e0102935218cdd367f1d2d51a3d8a4
Savic23 <aleksandarsavic23062003@gmail.com>
2025-10-03 22:19:13 +0200

    route get favorites

--- server/src/routes/favorites.ts
+++ server/src/routes/favorites.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { toggleFavorite } from "../controller/favorites.controller";
+import {
+  getUserFavorites,
+  toggleFavorite,
+} from "../controller/favorites.controller";
 import { requireUser } from "../middleware/requireUser";
 
 const router = Router();
 
 router.post("/toggle", requireUser, toggleFavorite);
+router.get("/", requireUser, getUserFavorites);
 
 export default router;
