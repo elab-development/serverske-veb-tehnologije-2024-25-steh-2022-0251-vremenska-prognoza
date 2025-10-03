266382f3c9d4ea4e4f3c7e8161c0f2ff51d418bb
Savic23 <aleksandarsavic23062003@gmail.com>
2025-10-03 22:15:44 +0200

    get favorites

--- server/src/controller/favorites.controller.ts
+++ server/src/controller/favorites.controller.ts
@@ -2,6 +2,7 @@ import { and, eq } from "drizzle-orm";
 import { Response } from "express";
 import { db } from "../db/db";
 import { favorites } from "../db/schema/favorites";
+import { news } from "../db/schema/news";
 import { AuthenticatedRequest } from "../middleware/requireAdmin";
 
 // Toggle favorite (add/remove)
@@ -33,5 +34,37 @@ export const toggleFavorite = async (
   } catch (err) {
     console.error("Error toggling favorite:", err);
     return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// Get all favorites for user
+export const getUserFavorites = async (
+  req: AuthenticatedRequest,
+  res: Response
+) => {
+  const userId = req.user?.id;
+  if (!userId) return res.status(401).json({ message: "Unauthorized" });
+
+  try {
+    const favs = await db
+      .select({
+        id: news.id,
+        title: news.title,
+        subtitle: news.subtitle,
+        content: news.content,
+        image: news.image,
+        authorId: news.authorId,
+        slug: news.slug,
+        createdAt: news.createdAt,
+        updatedAt: news.updatedAt,
+      })
+      .from(favorites)
+      .leftJoin(news, eq(favorites.newsId, news.id))
+      .where(eq(favorites.userId, userId));
+
+    res.json(favs);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
