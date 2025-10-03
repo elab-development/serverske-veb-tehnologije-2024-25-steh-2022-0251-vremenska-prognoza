67d9ab47437cb3cbf1dd175453f6b90bedf23f4e
RadeStojicic <radestojicicsd@gmail.com>
2025-10-03 23:07:50 +0200

    verifyOtp & resetPassword api's

--- server/src/controller/password.controller.ts
+++ server/src/controller/password.controller.ts
@@ -24,4 +24,51 @@ export const requestOtp = async (req: Request, res: Response) => {
   }
 };
 
+export const verifyOtp = async (req: Request, res: Response) => {
+  const { email, otp } = req.body;
+  if (!email || !otp)
+    return res.status(400).json({ message: "Email and OTP are required" });
+
+  try {
+    const data = await auth.api.checkVerificationOTP({
+      body: {
+        email,
+        otp,
+        type: "forget-password",
+      },
+    });
+
+    if (!data.success) return res.status(400).json({ message: "Invalid OTP" });
+
+    return res.json({ message: "OTP verified" });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Server error", error: String(error) });
+  }
+};
+
+export const resetPassword = async (req: Request, res: Response) => {
+  const { email, otp, password } = req.body;
+  if (!email || !otp || !password)
+    return res.status(400).json({ message: "All fields are required" });
+
+  try {
+    const data = await auth.api.resetPasswordEmailOTP({
+      body: { email, otp, password },
+    });
+
+    if (!data.success)
+      return res.status(400).json({ message: "Failed to reset password" });
+
+    return res.json({ message: "Password reset successfully" });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "Server error", error: String(error) });
+  }
+};
+
 export default router;
--- server/src/routes/password.ts
+++ server/src/routes/password.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { requestOtp } from "../controller/password.controller";
+import {
+  requestOtp,
+  resetPassword,
+  verifyOtp,
+} from "../controller/password.controller";
 
 const router = Router();
 
 router.post("/request-otp", requestOtp);
+router.post("/verify-otp", verifyOtp);
+router.post("/reset-password", resetPassword);
 
 export default router;
